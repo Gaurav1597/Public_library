1bcfd9a172779bb2f0ac615a267191acb4a20e8a
Gaurav Bhatia <GB021@atcltd.atc.org.nz>
2021-07-02 19:21:42 +1200

    Add project files.

---
+++ Resources.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Public_library
+{
+    internal class Resources
+    {
+        internal class Adpter_Books_Category_List
+        {
+            private Create_Book_Activity create_Book_Activity;
+            private List<Tbl_Books_Category> list_All_Category;
+            private Create_Category_Activity create_Category_Activity;
+            private List<Tbl_Books_Category> list_Category;
+
+            public Adpter_Books_Category_List(Create_Book_Activity create_Book_Activity, List<Tbl_Books_Category> list_All_Category)
+            {
+                this.create_Book_Activity = create_Book_Activity;
+                this.list_All_Category = list_All_Category;
+            }
+
+            public Adpter_Books_Category_List(Create_Category_Activity create_Category_Activity, List<Tbl_Books_Category> list_Category)
+            {
+                this.create_Category_Activity = create_Category_Activity;
+                this.list_Category = list_Category;
+            }
+        }
+
+        internal class Adpter_Books
+        {
+            private Create_Book_Activity create_Book_Activity;
+            private List<Tbl_Books> list_All_Books;
+
+            public Adpter_Books(Create_Book_Activity create_Book_Activity, List<Tbl_Books> list_All_Books)
+            {
+                this.create_Book_Activity = create_Book_Activity;
+                this.list_All_Books = list_All_Books;
+            }
+        }
+    }
+}
---
+++ SQLiteConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Public_library
+{
+    internal class SQLiteConnection
+    {
+        private string dpPath;
+
+        public SQLiteConnection(string dpPath)
+        {
+            this.dpPath = dpPath;
+        }
+
+        internal void CreateTable<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Delete(Tbl_Books item)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Insert(Tbl_Books tbl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
